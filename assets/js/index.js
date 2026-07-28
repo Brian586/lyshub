3ba1a1c44c1dd556d4492e5693e5f0195c67d44e
Brian586 <briannamutali586@gmail.com>
2026-07-29 00:48:29 +0300

    new updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 import services from "./data/services-data.js";
-import team from "./data/team-data.js";
 import projects from "./data/projects-data.js";
 import testimonials from "./data/testimonials-data.js";
 
@@ -49,49 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     // Services Section
 
-    // Team Section
-    const teamList = document.getElementById("team-list");
-
-    team.forEach((member) => {
-        const teamMember = document.createElement("div");
-        teamMember.className = "col-lg-4 col-md-6 col-sm-12 col-xs-12";
-
-        teamMember.innerHTML = `
-        <div class="team_box style_one">
-            <div class="team_box_outer">
-                <div class="member_image">
-                    <img src="${member.image}" alt="${member.name}">
-                </div>
-                <div class="about_member">
-                    <div class="share_media">
-                        <ul class="first">
-                        <li class="text">Links</li>
-                        <li><i class="fa fa-share-alt"></i></li>
-                        </ul>
-                        <ul>
-                        <li class="shar_alt"><i class="fa fa-share-alt"></i></li>
-                        <li><a href="${member.socialLinks.linkedin}" target="_blank"> <i class="fa fa-linkedin"> </i></a></li>
-                        <li><a href="${member.socialLinks.github}" target="_blank"> <i class="fa fa-github"> </i></a></li>
-                        <li><a href="${member.socialLinks.website}" target="_blank"> <i class="fa fa-link"> </i></a></li>
-                        </ul>
-                    </div>
-                    <div class="authour_details">
-                        <span>${member.position}</span>
-                        <h6>${member.name}</h6>
-                        <div class="button_view">
-                        <a href="${member.profile}" target="_blank" rel="nofollow" class="theme-btn one"> View Profile
-                        </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-            <div class="mr_bottom_30"></div>
-        `;
-        teamList.appendChild(teamMember);
-    });
-    // Team Section
-
     // Projects Section
     const projectsList = document.getElementById("projects-list");
 
@@ -124,6 +80,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Testimonials Section
     const testimonialsList = document.getElementById("testimonials-list");
+
+    if (testimonials.length === 0) {
+        const testimonialSection = testimonialsList.closest(".testimonial-section");
+
+        if (testimonialSection) {
+            testimonialSection.style.display = "none";
+        }
+    }
 
     testimonials.forEach((testimonial) => {
         const testimonialItem = document.createElement("div");
